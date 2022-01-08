178836297c7d35edfd99accf014a47a78d3bfd35
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2022-01-08 11:10:23 -0800

    Add files via upload

---
+++ cryptobot07-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\cryptobot07-Web-Deploy.pubxml -p:Password=6aNhfjqsjoWtJaJHW4JoXwSWcXdaXfM7rLDetLuiFDhvhfHMDxTCqrLTYHsl
+
